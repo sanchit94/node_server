271964c43dc49e584a31d80476ecf0a1513d6d29
Sanchit <sanchits174@gmail.com>
2019-01-24 02:10:12 +0530

    Added project to version control

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 3000
+let cardData;
+
+fs.readFile('./data/card-data.json', 'utf8', function (err, data) {
+	if(err) throw err;
+	cardData = JSON.parse(data)
+})
+
+app.get('/', (req, res) => {
+				res.set({
+							'Access-Control-Allow-Origin': ['*'],
+							'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+								})
+				res.json(cardData)
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
